de634cf47ba3bdfc97bedb1e591eb63a2ca12382
poula00saber <143388373+poula00saber@users.noreply.github.com>
2025-02-07 01:04:34 +0200

    Delete Poula Saber directory

--- Poula Saber/Drivable.java
+++
@@ -1,8 +0,0 @@
-public interface Drivable
-{
-
-    void startVehicle() ;
-    void stopVehicle() ;
-    void accelerateVehicle() ;
-
-}
--- Poula Saber/Upgradeable.java
+++
@@ -1,5 +0,0 @@
-public interface Upgradeable {
-
-    void upgradeEngine() ;
-
-}
