ff2256675a6e289c107de3a66524df647ea7603e
Mohammed Atef <mohammedatef822004@gmail.com>
2025-03-06 22:29:55 +0200

    Fatma Elkassaby Task

---
+++ .NET/Session 2/Session 2 Task/osc_.net_task1/osc_.net_task1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace osc_.net_task1 {
+
+    public struct Product {
+        private int id;
+        private string name;
+        private decimal price;
+        private int stock;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public decimal Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public int Stock
+        {
+            get { return stock; }
+            set { stock = value; }
+        }
+
+        public void restock(int newStock) {
+            this.stock += newStock;
+        }
+
+        public Product(int id,string name,decimal price,int stock) {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.stock=stock;
+        }
+    }
+
+    class Program {
+        static void Main() {
+            Warehouse Warehouse1 = new Warehouse();
+            Warehouse1.AddProduct(new Product(0,"product one",20,2));
+            Warehouse1.AddProduct(new Product(1,"product two", 30, 3));
+            Warehouse1.AddProduct(new Product(2,"product three", 140,4));
+            Warehouse1.AddProduct(new Product(3,"product four", 150,5));
+            Warehouse1.ProductRestocked += Warehouse1.ProductIncreaseNotification;
+            Warehouse1.LowStockWarning += Warehouse1.LowStockNotification;
+            Warehouse1.RestockProduct(2, 7);
+            Warehouse1.LowStockIdentification();
+            Console.WriteLine("\nID\t\t\tName\t\t\tPrice\t\t\tStock");
+            foreach (var item in Warehouse1) {
+                Console.WriteLine(item.Id+"\t\t\t"+item.Name+"\t\t"+item.Price+"\t\t\t"+item.Stock);
+            }
+            Console.WriteLine("\n\nTotal Value:" + Warehouse1.GetTotalVale());
+            Console.WriteLine("\n\n--------------------------------------Expensive Products ------------------------------");
+            Console.WriteLine("ID\t\t\tName\t\t\tPrice\t\t\tStock");
+            foreach (var item in Warehouse1.GetExpensiveProducts())
+            {
+                Console.WriteLine(item.Id + "\t\t\t" + item.Name + "\t\t" + item.Price + "\t\t\t" + item.Stock);
+            }
+        }
+    }
+}
---
+++ .NET/Session 2/Session 2 Task/osc_.net_task1/osc_.net_task1/Warehouse.cs
@@ -0,0 +1,83 @@
+﻿using osc_.net_task1;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace osc_.net_task1
+{
+    public delegate void StockAlertHandler(int id,int newStock);
+    class Warehouse : IEnumerable<Product>
+    {
+        public event StockAlertHandler ProductRestocked;
+        public event StockAlertHandler LowStockWarning;
+        private List<Product> products = new List<Product>();
+        public void AddProduct(Product P) {
+            products.Add(P);
+        }
+        public void RestockProduct(int id,int newStock)
+        {
+            Product temp;
+            for (int i = 0; i < products.Count; i++)
+            {
+
+                if (products[i].Id == id)
+                {
+                    temp = products[i];
+                    temp.restock(newStock);
+                    products[i] = temp;
+                    ProductRestocked?.Invoke(id, newStock);
+                    break;
+                }
+            }
+        }
+        public void LowStockIdentification()
+        {
+            foreach (var item in products)
+            {
+                if (item.Stock <= 3) {
+                   LowStockWarning?.Invoke(item.Id, item.Stock);
+                }
+            }
+        }
+        public IEnumerator<Product> GetEnumerator()
+        {
+            foreach (var item in products)
+            {
+                yield return item;
+            }
+        }
+        public IEnumerable<Product> GetExpensiveProducts()
+        {
+            foreach (var item in products)
+            {
+                if(item.Price > 100)
+                   yield return item;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        public void LowStockNotification(int id,int stock)
+        {
+            Console.WriteLine("\nThe product with id (" + id + "):\nHave only " + stock+"left.");
+        }
+        public void ProductIncreaseNotification(int id, int stock)
+        {
+            Console.WriteLine("\nThe product with id (" + id + "):\nIt is amount have been increased and become " + stock);
+        }
+    }
+}
+ 
+static class TotalValue
+{
+    public static decimal GetTotalVale(this IEnumerable<Product> p)
+    {
+        decimal result = 0;
+        foreach (var item in p)
+        {
+            result += item.Price * item.Stock;
+        }
+        return result;
+    }
+}
